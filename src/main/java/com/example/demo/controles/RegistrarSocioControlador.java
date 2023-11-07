362c403869f142b05239717fab830cbd4e6b2781
Jonatancon <jhonatanlora2011@gmail.com>
2023-11-07 14:35:30 -0500

    feat: creada la funcionalidad de crear socio.

--- src/main/java/com/example/demo/controles/RegistrarSocioControlador.java
+++ src/main/java/com/example/demo/controles/RegistrarSocioControlador.java
@@ -1,5 +1,7 @@
 package com.example.demo.controles;
 
+import com.example.demo.modelo.Club;
+import com.example.demo.modelo.Socio;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
@@ -8,6 +10,7 @@ import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.TextField;
 
 import javax.print.DocFlavor;
+import javax.swing.*;
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -21,13 +24,57 @@ public class RegistrarSocioControlador implements Initializable {
     @FXML
     private TextField creditoInicial;
 
+    private Club club = new Club();
+
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         ObservableList<String> items = FXCollections.observableArrayList();
         items.add("VIP");
         items.add("REGULAR");
         tipoMembresia.setItems(items);
+    }
 
+    @FXML
+    public void guardarSocio() {
+         Socio socio = validarCampos();
+
+        if (validarCupos() && socio != null) {
+            club.getListaSocios().add(socio);
+            JOptionPane.showMessageDialog(null, "Socio creado");
+        }
+        JOptionPane.showMessageDialog(null, "No se pudo crear el Socio");
+    }
+
+    private boolean validarCupos() {
+        if (club.getListaSocios().size() > 30) {
+            JOptionPane.showMessageDialog(null, "Ya se agotaron los cupos del club");
+        }
+        return club.getListaSocios().size() < 30;
+    }
+
+    private Socio validarCampos () {
+        String nombre = nombreCompleto.getText();
+        String suscripcion = tipoMembresia.getValue();
+
+        if (creditoInicial.getText().isEmpty() || creditoInicial.getText().isBlank() ||
+                identificacion.getText().isEmpty() || identificacion.getText().isBlank() ||
+                nombre.isEmpty() || nombre.isBlank() ||
+                suscripcion.isEmpty() || suscripcion.isBlank()) {
+
+            JOptionPane.showMessageDialog(null, "Debe llenar todos los campos");
+
+            return null;
+        }
+        float fondos = Float.parseFloat(creditoInicial.getText());
+        int cedula = Integer.parseInt(identificacion.getText());
+
+        if (fondos < 30000) { //revisar no me acuerdo de cuanto es segun la membresia.
+            JOptionPane.showMessageDialog(null, "Fondos insuficientes o se sobrepaso");
+
+            return null;
+        }
+
+        return new Socio(fondos, suscripcion, cedula, nombre);
     }
 }
 
